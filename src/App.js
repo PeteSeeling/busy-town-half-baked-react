8f21abf79133b76b23fb219fcdb1ca20a45ec7a4
Pete Seeling <peteseeling@gmail.com>
2022-01-26 19:23:30 -0600

    vehicles done. work on vehicle list

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   // alienSize should be a number that starts out as 10
   const [alienSize, setAlienSize] = useState(10);
   // traffic is complicated. It should be an array of strings that starts out as ['car', 'truck']
-  const [vehiclesArray, setVehiclesArray] = useState(['car', 'truck', 'bus', 'van']);
+  const [vehiclesArray, setVehiclesArray] = useState(['car', 'truck', 'bus', 'motorcycle']);
 
   return (
     <div className="App">
@@ -54,7 +54,7 @@ function App() {
       <div className='buttons'>
         {/* This part is weird */}
         {/* On click, you should set the traffic in state to be a copy of the same array that's already in state, but immutably add a 'car' to the end */}
-        <button>Car</button>
+        <button onClick={setVehiclesArray([...vehiclesArray, 'car'])}>Car</button>
         {/* On click, you should set the traffic in state to be a copy of the same array that's already in state, but immutably add a 'bus' to the end */}
         <button>Bus</button>
         {/* On click, you should set the traffic in state to be a copy of the same array that's already in state, but immutably add a 'truck' to the end */}
