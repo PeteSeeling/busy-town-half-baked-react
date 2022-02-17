99b427daacf053f3df2230ed5d405ec8a93ce9b3
Pete Seeling <peteseeling@gmail.com>
2022-02-17 13:58:59 -0600

    test written. needs to pass

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import TrafficLight from './TrafficLight';
+
 
 test('Yegads!', () => {
   render(<App />);
   const linkElement = screen.getByText(/Yegads!/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('', async () => {
+  render(<TrafficLight />);
+  const buttonElement = screen.getByDisplayValue('yellow');
+
+  fireEvent.click(buttonElement, { target: { value: 'yellow' } });
+
+  const colorElement = await screen.findByText(/yellow/i);
+  expect(colorElement).toBeInTheDocument();
+
 });
